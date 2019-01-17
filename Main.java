495ca80a6d71ae851b453686d4ae1408a9b063ec
jhLee <kim0lil@naver.com>
2019-01-17 13:16:49 +0900

    2019-01-17

--- Main.java
+++ Main.java
@@ -1,5 +1,3 @@
-package TT;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,6 +9,7 @@ import java.util.Scanner;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 	
@@ -27,7 +26,6 @@ public class Main {
 		 * ---------------------
 		 * 1	9498	시험 성적
 		 */
-
 		/*
 		getMatcher(sc);
 		*/
@@ -38,7 +36,6 @@ public class Main {
 		 * ---------------------
 		 * 2	10817	세 수
 		 */
-
 		/*
 		getTreepleMiddleValue(sc);
 		*/
@@ -49,7 +46,6 @@ public class Main {
 		 * ---------------------
 		 * 3	10871	X보다 작은 수
 		 */
-
 		/*
 		getLessX(sc);
 		*/
@@ -71,7 +67,6 @@ public class Main {
 		 * ---------------------
 		 * 5    4344       평균은 넘겠지
 		 */
-
 		/*
 		getAverageOver(sc);
 		*/
@@ -93,7 +88,6 @@ public class Main {
 		 * ---------------------
 		 * 1	4673	셀프 넘버
 		 */
-
 		/*
 		getNotSelfNumber();
 		*/
@@ -104,29 +98,191 @@ public class Main {
 		 * ---------------------
 		 * 2	1065	한수
 		 */	
-		
 		/*
 		getSinleDefValue(sc);
 		*/
 		
-		getPrintStart(sc);
-
+		/**
+		 * ---------------------
+		 * 단계    문제 번호     제목
+		 * ---------------------
+		 * 3	2448	별 찍기 - 11
+		 */	
+		/*
+		getPrintStar(sc);
+		*/
+		
 		/**
 		 * ---------------------
 		 * 단계    문제 번호     제목
 		 * ---------------------
 		 * 1	1152	단어의 개수
 		 */
-
 		/*
 		getTokenCount(sc);
 		*/
 		
+		/**
+		 * ---------------------
+		 * 단계    문제 번호     제목
+		 * ---------------------
+		 * 2	2577	숫자의 개수
+		 */
+		/*
+		getCountNumber(sc);
+		*/
+		
+		/**
+		 * ---------------------
+		 * 단계    문제 번호     제목
+		 * ---------------------
+		 * 3	8958	OX퀴즈
+		 */
+		/*
+		getOXQuiz(sc);
+		*/
+		
+		/**
+		 * ---------------------
+		 * 단계    문제 번호     제목
+		 * ---------------------
+		 * 4	2920	음계
+		 */
+		/*
+		getSortCheck(sc);
+		*/
+		
+		/**
+		 * ---------------------
+		 * 단계    문제 번호     제목
+		 * ---------------------
+		 * 5	10039	평균 점수
+		 */
+		/*
+		getSelectiveAverage(sc);
+		*/
 		long end = System.currentTimeMillis();
 
 		System.out.printf("실행 시간 : %.3f(초)",(end-start) / 1000.0);
 	}
 	
+	/**
+	 * Test ::
+	 * 10
+	 * 65
+	 * 100
+	 * 30
+	 * 95
+	 * 
+	 * @param sc Scanner
+	 */
+	public static void getSelectiveAverage(Scanner sc) {
+
+		int value = Arrays.asList(sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt(), sc.nextInt())
+					      .stream()
+					      .map(e->e<40?40:e)
+					      .collect(Collectors.averagingInt(e->e))
+					      .intValue();
+
+
+		System.out.println(value);
+	}
+	
+	/**
+	 * Test ::
+	 * 1 2 3 4 5 6 7 8
+	 * 
+	 * @param sc Scanner
+	 */
+	public static void getSortCheck(Scanner sc) {
+		
+		String sLine = sc.nextLine();
+
+		String sAsc  = _getPlainText(_getStringStream(sLine).sorted());
+		String sDesc = _getPlainText(_getStringStream(sLine).sorted((x,y)->y.charAt(0)-x.charAt(0)));
+		
+		if(sLine.equals(sAsc)) {
+			
+			System.out.println("ascending");
+		} else if(sLine.equals(sDesc)) {
+			
+			System.out.println("descending");
+		} else {
+			
+			System.out.println("mixed");
+		}
+	}
+	
+	public static String _getPlainText(Stream stream) {
+		
+		return stream.collect(Collectors.toList()).toString().replaceAll("[\\[,\\]]", "");
+	}
+	
+	public static Stream<String> _getStringStream(String str) {
+		
+		return Arrays.stream(str.split(" "));
+	}
+	
+	/**
+	 * Test 
+	 * 5
+	 * OOXXOXXOOO
+	 * OOXXOOXXOO
+	 * OXOXOXOXOXOXOX
+	 * OOOOOOOOOO
+	 * OOOOXOOOOXOOOOX
+	 * 
+	 * @param sc Scanner
+	 */
+	public static void getOXQuiz(Scanner sc) {
+		int loop = sc.nextInt();
+		
+		List<String> list = getStringListForScanner(loop, sc);
+		
+		list.stream().forEach(Main::_getOXQuiz);
+	}
+	
+	public static void _getOXQuiz(String str) {
+
+		int iSize = str.length()+1;
+		int iScore = 0;
+		int iAcc = 0;
+		
+		while(iSize-->1)
+			iScore += iAcc = str.substring(iSize-1, iSize).equals("X") ? 0 : iAcc+1;
+		
+
+		System.out.println(iScore);
+	}
+	
+	/**
+	 * Test ::
+	 * 135
+	 * 662
+	 * 412
+	 * 
+	 * @param sc Scanner
+	 */
+	public static void getCountNumber(Scanner sc) {
+		
+		int result = sc.nextInt() * sc.nextInt() * sc.nextInt();
+		
+		String sIn = (Integer.toString(result) + "0123456789");
+		
+		Arrays.stream(sIn.split(""))
+		      .collect(Collectors.groupingBy(t->t))
+		      .values()
+		      .forEach(e->System.out.println(e.size()-1));
+	}
+	
+	
+	/**
+	 * Test ::
+	 * Hello World TokenCount
+	 * 
+	 * @param sc Scanner
+	 * @throws IOException BufferReader Exception
+	 */
 	public static void getTokenCount(Scanner sc) throws IOException {
 		
 		int value = new StringTokenizer(new BufferedReader(new InputStreamReader(System.in)).readLine()).countTokens();
@@ -135,20 +291,88 @@ public class Main {
 		System.out.print(value);
 	}
 	
-	public static void getPrintStart (Scanner sc) {
+	/**
+	 * Test ::
+	 * 24
+	 * 
+	 * @param sc Scanner
+	 */
+	public static void getPrintStar (Scanner sc) {
 		
-		int iLine, iLoop, iCell;  
+		int container_size = sc.nextInt(); 
+
+		_getPrintStar(null, 1, container_size/3);
+	}
+	
+	public static void _getPrintStar (int[][] container, int iSLV, int iCLV) {
 		
-		iCell = (iLoop = iLine = sc.nextInt())*2;
+		int iHeight,
+		    iWidth,
+		    iMeddle,
+		    iExPoint,
+		    iExLine;
 		
-		while(iLoop-->0) {
+		iExLine = (iExPoint = ((iMeddle = (iWidth = (iHeight = iSLV * 3) * 2) / 2 - 1) + 1) / 2);
+		
+		int[][] new_container = new int[iHeight][iWidth];
+		
+		if(iSLV == 1) {
+
+			new_container[0][iMeddle]   = 1;
+			new_container[1][iMeddle-1] = 1;
+			new_container[1][iMeddle+1] = 1;
 			
+			int cks = iMeddle+3;
+			
+			while(cks-->iMeddle-2)
+				new_container[2][cks] = 1;
+			
+			container = new_container;
+		} else {
+			
+			for(int x = 0 ; x < container.length ; x++) {
+
+				for(int y = 0 ; y < container[x].length ; y++ ){
+					
+					new_container[x][y+iExPoint]      = container[x][y];
+					new_container[iExLine][y]         = container[x][y];
+					new_container[iExLine][iHeight+y] = container[x][y];
+				}
+
+				iExLine++;
+			}
+
+			container = new_container;
 		}
 		
-		
+		if(iSLV >= iCLV) {
+			
+			StringBuilder sb = new StringBuilder();
+			
+			for(int x = 0 ; x < container.length ; x++) {
+				
+				for(int y = 0 ; y < container[x].length ; y++) {
+					
+					sb.append(container[x][y] == 0 ? " " : "*");
+				}
+				
+				sb.append("\n");
+			}
+			
+			
+			System.out.println(sb.toString());
+		} else {
+
+			_getPrintStar(container, iSLV*2, iCLV);
+		}
 		
 	}
 	
+	/**
+	 * Test :: 101
+	 * 
+	 * @param sc Scanner
+	 */
 	public static void getSinleDefValue (Scanner sc) {
 		
 		int loop = sc.nextInt();
@@ -383,13 +607,18 @@ public class Main {
 		List<T> list = new ArrayList<>();
 		
 		while(loop --> 0) {
-
+			
 			list.add(scannerCallback.getValue(sc));
 		}
 		
 		return list;
 	}
 
+	public static List<String> getStringListForScanner (int loop, final Scanner sc) {
+		sc.nextLine();
+		return getListForScanner(loop, sc, String.class, scanner -> scanner.nextLine());
+	}
+	
 	public static List<Integer> getIntListForScanner (int loop, final Scanner sc) {
 		
 		return getListForScanner(loop, sc, Integer.class, scanner -> scanner.nextInt());
